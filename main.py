b22a8ad25ed6ef8c995b6d5f53c99ba652e7dae7
adewotta <dylanwes@buffalo.edu>
2022-03-15 22:04:50 -0400

    Updates .gitignore, added error checking to main.py

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ import urllib.request
 import os, time, json
 
 def downloadDeltarune(KEY,uploadID,name):
-    r = requests.get("https://itch.io/api/1/%s/upload/%s/download" % (KEY,uploadID))
-    url = json.loads(r.text)['url']
-    urllib.request.urlretrieve(url, os.path.abspath("./(%s)%s.zip" % (uploadID,name)))
+    try:
+        r = requests.get("https://itch.io/api/1/%s/upload/%s/download" % (KEY,uploadID))
+        url = json.loads(r.text)['url']
+        urllib.request.urlretrieve(url, os.path.abspath("./(%s)%s.zip" % (uploadID,name)))
+        return True
+    except:
+        return False
 
 def checkForUpdates(KEY):
     #Store the upload ID of the games we have already downloaded into this dictioanry
@@ -22,9 +26,16 @@ def checkForUpdates(KEY):
         file = open("versionList.txt","w")
         file.close()
     
-    
-    source = requests.get("https://tobyfox.itch.io/deltarune").text
-    soup = bs.BeautifulSoup(source, 'html.parser')
+    try:
+        source = requests.get("https://tobyfox.itch.io/deltarune")
+        if source.status_code != 200:
+            print("Error: Bad Status Code")
+            return False
+    except:
+        print("Error connecting to https://tobyfox.itch.io/deltarune")
+        return False
+        
+    soup = bs.BeautifulSoup(source.text, 'html.parser')
     #Download each version
 
     uploads = soup.find_all(class_="upload")
@@ -34,22 +45,39 @@ def checkForUpdates(KEY):
         if uploadID not in VersionList:
             print("New Version Found: (%s)%s" % (uploadID,title))
             #download unarchived vesion of the game
-            downloadDeltarune(KEY,uploadID,title)
-            #Update Version List
-            VersionList[uploadID] = ""
-            #Save new version list to file
-            versionListJson = json.dumps(VersionList)
-            file = open("versionList.txt", "w")
-            file.write(versionListJson)
-            file.close()
-            print("Downloaded: (%s)%s" % (uploadID,title))
+            if downloadDeltarune(KEY,uploadID,title):
+                #Update Version List
+                VersionList[uploadID] = ""
+                #Save new version list to file
+                versionListJson = json.dumps(VersionList)
+                file = open("versionList.txt", "w")
+                file.write(versionListJson)
+                file.close()
+                print("Downloaded: (%s)%s" % (uploadID,title))
+            else:
+                print("Please check API key ")
+    return True
 
 if __name__ == "__main__":
+
+#Check for file privilages
+    try:
+        f = open("key.txt","w+")
+        f.close()
+    except:
+        print("Could not open file, permission denied")
+        exit(1)
+
+    #Get users API key
     f = open("key.txt","r",encoding = 'utf-8')
     KEY = f.read()
     f.close()
+
     while 1==1:
         print("Checking for updates...")
-        checkForUpdates(KEY)
-        print("Finished checking for updates")
+        if checkForUpdates(KEY):
+
+            print("Finished checking for updates")
+        else:
+            print("Error checking for updates")
         time.sleep(600)
